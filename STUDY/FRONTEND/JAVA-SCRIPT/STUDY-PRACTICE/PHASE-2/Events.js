6144b99b17ae39061a62bdcc08480002f7b2b66a
Pratim <pratimhalder04@gmail.com>
2025-08-25 22:41:44 +0530

    change event

--- STUDY/FRONTEND/JAVA-SCRIPT/STUDY-PRACTICE/PHASE-2/Events.js
+++ STUDY/FRONTEND/JAVA-SCRIPT/STUDY-PRACTICE/PHASE-2/Events.js
@@ -1,6 +1,23 @@
+// Text Event
 let A = document.querySelector("h1");
 
-A.addEventListener("dblclick", function(){
+A.addEventListener("click", function () {
     A.innerHTML = "<i>Ahh! You Clicked</i>";
     A.style.color = "red";
+});
+
+// Input Event
+let ip = document.querySelector(".a");
+
+ip.addEventListener("input",function(e){
+    if (e.data !== null) {
+        console.log(e.data);
+    }
+});
+
+// Change Event
+let sel = document.querySelector(".EL");
+
+sel.addEventListener("change",function (dets) {
+    console.log(dets.target.value);
 });
