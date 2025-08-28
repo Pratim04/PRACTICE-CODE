77858b2069f908cb628c0c7d308ee982a15bec99
Pratim <pratimhalder04@gmail.com>
2025-08-28 21:36:36 +0530

    EventBubbling

---
+++ STUDY/FRONTEND/JAVA-SCRIPT/STUDY-PRACTICE/PHASE-2/EVENTS/bubbling.js
@@ -0,0 +1,5 @@
+let list = document.querySelector("#LIA");
+
+list.addEventListener("click",function (e) {
+    e.target.classList.toggle("LA");
+})
--- STUDY/FRONTEND/JAVA-SCRIPT/STUDY-PRACTICE/PHASE-2/EVENTS/mouseMove.js
+++ STUDY/FRONTEND/JAVA-SCRIPT/STUDY-PRACTICE/PHASE-2/EVENTS/mouseMove.js
@@ -1,8 +1,13 @@
 
 let Box = document.querySelector("#box");
 
+// MouseMove : 
+
 window.addEventListener("mousemove",function (dets) {
     console.log(dets.clientX ,dets.clientY);
     Box.style.top = dets.clientY + "Px";
     Box.style.left = dets.clientX + "Px";
 });
+
+// This 'dets' in the event funtion is the Event Object
+
