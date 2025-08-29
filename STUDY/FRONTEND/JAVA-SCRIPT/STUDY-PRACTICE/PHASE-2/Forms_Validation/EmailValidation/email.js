13418696770fc89b48358808d9ac3859ff9efe45
Pratim <pratimhalder04@gmail.com>
2025-08-30 02:28:32 +0530

    Forms and Form Validation

---
+++ STUDY/FRONTEND/JAVA-SCRIPT/STUDY-PRACTICE/PHASE-2/Forms_Validation/EmailValidation/email.js
@@ -0,0 +1,38 @@
+let EmailInp = document.querySelector("#email");
+let passwordInp = document.querySelector("#password");
+let form = document.querySelector("form");
+
+form.addEventListener("submit",function (dets) {
+    dets.preventDefault();
+
+    document.querySelector("#emailError").textContent = "";
+    document.querySelector("#passError").textContent = "";
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/;
+    
+    let isValid = true;
+
+    let emailAns = emailRegex.test(EmailInp.value);
+    let passwordAns = passwordRegex.test(passwordInp.value);
+
+    if(!emailAns) {
+        document.querySelector("#emailError").textContent = "Email is invalid";
+        isValid = false;
+    }
+    
+    if(!passwordAns) {
+        document.querySelector("#passError").textContent = "Password is invalid";
+        isValid = false;
+    }
+
+    if (isValid) {
+        document.querySelector("#resultMessage").textContent = "Everything is OK!"
+    } 
+    else {
+        document.querySelector("#resultMessage").textContent = ""
+    }
+
+
+
+});
