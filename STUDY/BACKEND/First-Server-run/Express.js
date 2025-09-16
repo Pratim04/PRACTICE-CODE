42e6cbfc49ac2dce653ae34851f8147838a1f31d
Pratim <pratimhalder04@gmail.com>
2025-09-16 22:18:55 +0530

    basic done of nide express crud

--- STUDY/BACKEND/First-Server-run/Express.js
+++ STUDY/BACKEND/First-Server-run/Express.js
@@ -33,8 +33,55 @@ app.get('/profile',(req,res) =>{
     res.send('New Profile Page');
 });
 
+
+app.get('/register',(req,res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req,res)=>{
+    
+    const { username, email, password } = req.body
+    
+    const newUser = await userModel.create({
+        username:username,
+        email:email,
+        password:password
+    });
+    
+    res.send(newUser);
+    
+});
+
 app.post('/get-form-data',(req,res)=>{
-    console.log(req.body)
+    console.log(req.body);
     res.send('data received');
 });
+
+// find() : 
+app.get('/getUsers',(req,res) => {
+    userModel.find().then((user)=>{
+        res.send(user);
+    })
+});
+
+// Update() :
+app.get('/update-User',async (req,res)=>{
+    await userModel.findOneAndUpdate({
+        username: 'v'
+    }, {
+        email:"Vinod@gmail.com"
+    });
+
+    res.send('User Updated');
+});
+
+// Delete() :
+app.get('/deleteUser',async (req,res) => {
+    await userModel.findOneAndDelete({
+        username: "v"
+    });
+
+    res.send('User Deleted');
+});
+
 app.listen(5000,console.log('Server Running'));
