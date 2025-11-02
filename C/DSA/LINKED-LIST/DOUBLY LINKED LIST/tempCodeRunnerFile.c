37f04edb9d8f3d118db8ddd311765d539f555a3d
Pratim <pratimhalder04@gmail.com>
2025-11-02 19:53:39 +0530

    PY

--- C/DSA/LINKED-LIST/DOUBLY LINKED LIST/tempCodeRunnerFile.c
+++ C/DSA/LINKED-LIST/DOUBLY LINKED LIST/tempCodeRunnerFile.c
@@ -1,11 +1,1 @@
-head->prev = NULL;
-    head->data = 45;
-    head->next = ptr;
-
-    ptr->prev = head;
-    ptr->data = 66;
-    ptr->next = ptr1;
-
-    ptr1->prev = ptr;
-    ptr1->data = 58;
-    ptr1->next = NULL;
+head
--- PYTHON/test/1.py
+++ PYTHON/test/1.py
@@ -1,15 +1,6 @@
-def is_multiple (n: int, m: int) -> bool:
-    """Returns True if n is a multiple of m, False otherwise.
+def sum_to(n: int) -> int:
+    if n == 1:
+        return 1
+    return sum_to(n - 1) + n
 
-    >>> is_multiple(10, 5)
-    True
-    >>> is_multiple(10, 3)
-    False
-    >>> is_multiple(0, 1)
-    True
-    >>> is_multiple(5, 0)
-    False
-    """
-    if m == 0:
-        return False
-    return n % m == 0
+print(sum_to(4))
