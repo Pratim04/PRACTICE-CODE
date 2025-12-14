1bb63d14a23db677fbb1f1ec22e3664c35273dab
Pratim04 <169261223+Pratim04@users.noreply.github.com>
2025-12-14 10:37:05 +0530

    adding array

--- JAVA/BASIC/ArrayExample.java
+++ JAVA/BASIC/ArrayExample.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class ArrayExample {
-    public static void main(String[] args) {
-        int[] numbers = { 10, 20, 30, 40, 50 };
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+    public static void main() {
+
+        System.out.print("Enter the array size: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] numbers = new int[n];
+        
+        System.out.print("Enter array elements: ");
+
+        for (int i = 0;i<n;i++) {
+            numbers[i] = sc.nextInt(); 
         }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        sc.close();
     }
 }
